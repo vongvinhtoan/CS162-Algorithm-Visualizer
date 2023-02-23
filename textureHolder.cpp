2782b5c2b236443fa4c705d4965de9fe330b988f
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-02-23 22:44:05 +0700

    texture holder error handling

--- textureHolder.cpp
+++ textureHolder.cpp
@@ -4,11 +4,11 @@ void TextureHolder::load(Textures::ID id, const std::string& filename)
 {
     std::unique_ptr<sf::Texture> texture(new sf::Texture());
     if(!texture->loadFromFile(filename)) 
-    {
         throw std::runtime_error("TextureHolder::load - Failed to load "  + filename);
-    }
 
-    mTextureMap.insert(std::make_pair(id, std::move(texture)));
+    auto inserted = mTextureMap.insert(std::make_pair(id, std::move(texture)));
+    if(inserted.second == false) 
+        throw std::logic_error("TextureHolder::load - Inserted multiple times");
 }
 
 sf::Texture& TextureHolder::get(Textures::ID id)
