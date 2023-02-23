e581efbdf2bccca4d0e0b0b1ac939d953fdd6ab2
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-02-24 00:15:13 +0700

    overload load function

--- ResourceHolder.hpp
+++ ResourceHolder.hpp
@@ -9,6 +9,8 @@ class ResourceHolder
 {
     public:
         void load(Identifier id, const std::string& filename);
+        template <typename Parameter>
+        void load(Identifier id, const std::string& filename, const Parameter& secondParam);
 
         Resource& get(Identifier id);
         const Resource& get(Identifier id) const;
