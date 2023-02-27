1282a682c2f5bb7abe927912946132918fa6030e
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-02-27 10:28:42 +0700

    Update Flag.cpp

--- Flag.cpp
+++ Flag.cpp
@@ -1,16 +1,5 @@
 #include "Flag.hpp"
 
-Textures::ID toTextureID(Flag::Type type)
-{
-    switch (type)
-    {
-        case Flag::Flag1:
-            return Textures::Flag1;
-        case Flag::Flag2:
-            return Textures::Flag2;
-    }
-}
-
 Flag::Flag(Type type, const TextureHolder &texture) : 
 mType(type),
 mSprite(texture.get(toTextureID(type)))
@@ -21,4 +10,18 @@ mSprite(texture.get(toTextureID(type)))
 void Flag::drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const
 {
     target.draw(mSprite, states);
+}
+
+Textures::ID toTextureID(Flag::Type type)
+{
+    switch (type)
+    {
+        case Flag::Flag1:
+            return Textures::Flag1;
+        case Flag::Flag2:
+            return Textures::Flag2;
+        default:
+            throw std::runtime_error("an noi xa lo sao con noi di?");
+            break;
+    }
 }
