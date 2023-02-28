4f2a840aed8e8714523756620fa2d2ca70f55d23
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-02-28 12:09:56 +0700

    pruned game structure

--- game.cpp
+++ game.cpp
@@ -2,21 +2,8 @@
 
 Game::Game() : 
 mWindow(sf::VideoMode(640, 480), "Algorithm Visualizer"),
-// mTexture(),
-// mPlayer(),
-// mIsMovingUp(false),
-// mIsMovingDown(false),
-// mIsMovingLeft(false),
-// mIsMovingRight(false),
-// playerSpeed(200.f),
 mWorld(mWindow)
 {
-    // mTexture.load(Textures::Flag1, "Media/Textures/game_flag.bmp");
-    // mTexture.load(Textures::Flag2, "Media/Textures/game_flag.bmp");
-    
-    // mPlayer.setTexture(mTexture.get(Textures::Flag1));
-    // mPlayer.setPosition(100.f, 100.f);
-
     mWindow.setFramerateLimit(60);
     mWindow.setVerticalSyncEnabled(true);
 }
@@ -46,12 +33,6 @@ void Game::processEvents()
     {
         switch (event.type)
         {
-            // case sf::Event::KeyPressed:
-            //     handlePlayerInput(event.key.code, true);
-            //     break;
-            // case sf::Event::KeyReleased:
-            //     handlePlayerInput(event.key.code, false);
-            //     break;
             case sf::Event::Closed:
                 mWindow.close();
                 break;
@@ -61,17 +42,6 @@ void Game::processEvents()
 
 void Game::update(sf::Time dt)
 {
-    // sf::Vector2f movement(0.f, 0.f);
-    // if (mIsMovingUp)
-    //     movement.y -= playerSpeed;
-    // if (mIsMovingDown)
-    //     movement.y += playerSpeed;
-    // if (mIsMovingLeft)
-    //     movement.x -= playerSpeed;
-    // if (mIsMovingRight)
-    //     movement.x += playerSpeed;
-
-    // mPlayer.move(movement * deltaTime.asSeconds());
     mWorld.update(dt);
 }
 
@@ -84,14 +54,7 @@ void Game::render()
     mWindow.display();
 }
 
-// void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
-// {
-//     if (key == sf::Keyboard::W)
-//         mIsMovingUp = isPressed;
-//     else if (key == sf::Keyboard::S)
-//         mIsMovingDown = isPressed;
-//     else if (key == sf::Keyboard::A)
-//         mIsMovingLeft = isPressed;
-//     else if (key == sf::Keyboard::D)
-//         mIsMovingRight = isPressed;
-// }
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed)
+{
+    
+}
--- game.hpp
+++ game.hpp
@@ -10,21 +10,17 @@ class Game
 public:
     Game();
     void run();
+    
 private:
     void processEvents();
     void update(sf::Time deltaTime);
     void render();
-    // void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
+
 private:
     sf::RenderWindow    mWindow;
-    // TextureHolder       mTexture;
-    // sf::Sprite          mPlayer;
     World               mWorld;
-    // bool    mIsMovingUp;
-    // bool    mIsMovingDown;
-    // bool    mIsMovingLeft;
-    // bool    mIsMovingRight;
-    // float   playerSpeed;
+
 private:
     const sf::Time TimePerFrame = sf::seconds(1.f / 60.f);
 };
