55e86b9e2cc0746f13f918807337752f31c3020f
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-03-03 01:28:01 +0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -13,7 +13,7 @@ void Player::handleEvent(const sf::Event& event, CommandQueue& commands)
      && event.key.code == sf::Keyboard::P)
     {
         Command output;
-        output.category = Category::PlayerFlag;
+        output.category = Category::PlayerFlag | Category::AlliedFlag;
         output.action = [] (SceneNode& s, sf::Time)
         {
             std::cout << s.getPosition().x << "," 
@@ -25,13 +25,27 @@ void Player::handleEvent(const sf::Event& event, CommandQueue& commands)
 
 void Player::handleRealtimeInput(CommandQueue& commands)
 {
+    float playerSpeed = 100.f;
     
-    float playerSpeed = 30.f;
+    Command moveLeft;
+    moveLeft.category = Category::PlayerFlag;
+    moveLeft.action = derivedAction<Flag>(FlagMover(-playerSpeed, 0.f));
+    Command moveRight;
+    moveRight.category = Category::PlayerFlag;
+    moveRight.action = derivedAction<Flag>(FlagMover(playerSpeed, 0.f));
+    Command moveUp;
+    moveUp.category = Category::PlayerFlag;
+    moveUp.action = derivedAction<Flag>(FlagMover(0.f, -playerSpeed));
+    Command moveDown;
+    moveDown.category = Category::PlayerFlag;
+    moveDown.action = derivedAction<Flag>(FlagMover(0.f, playerSpeed));
+
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-    {
-        Command moveLeft;
-        moveLeft.category = Category::PlayerFlag;
-        moveLeft.action = derivedAction<Flag>(FlagMover(-playerSpeed, 0.f));
         commands.push(moveLeft);
-    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        commands.push(moveRight);
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        commands.push(moveUp);
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        commands.push(moveDown);
 }
