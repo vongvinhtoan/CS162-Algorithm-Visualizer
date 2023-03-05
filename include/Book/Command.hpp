905afc3c9d4de5ea6785d215d01858dbd60267c7
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-03-05 15:24:16 +0700

    integrate command category

--- include/Book/Command.hpp
+++ include/Book/Command.hpp
@@ -3,6 +3,18 @@
 #include <SFML/Graphics.hpp>
 #include <functional>
 
+namespace Category
+{
+    enum Type
+    {
+        None            = 0,
+        Scene           = 1 << 0,
+        PlayerFlag      = 1 << 1,
+        AlliedFlag      = 1 << 2,
+        EnemyFlag       = 1 << 3
+    };
+}
+
 class SceneNode;
 
 struct Command
