38c13d6cbbde4941435127c3efb8fb1bbe866b1c
toanvv.ti.1922@gmail.com <toanvv.ti.1922@gmail.com>
2023-03-06 20:26:37 +0700

    bring derivedAction from Player.hpp to Command.hpp

--- include/Book/Player.hpp
+++ include/Book/Player.hpp
@@ -12,19 +12,6 @@ class Player
         void     handleRealtimeInput(CommandQueue& commands);
 };
 
-template <typename GameObject, typename Function>
-std::function<void(SceneNode&, sf::Time)>
-    derivedAction(Function fn)
-{
-    return [=] (SceneNode& node, sf::Time dt)
-    {
-        // Check if cast is safe
-        assert(dynamic_cast<GameObject*>(&node) != nullptr);
-        // Downcast node and invoke function on it
-        fn(static_cast<GameObject&>(node), dt);
-    };
-}
-
 struct FlagMover
 {
     FlagMover(float vx, float vy)
